5c9822b8679f15327b4ca2deddaaa95444222c78
Nikita Tikhonov <pofofwar@gmail.com>
2022-02-02 16:23:37 +0300

    add splash screen and app icon for IOS and Android

--- android/app/src/main/java/com/dietdoctorassignment/MainActivity.java
+++ android/app/src/main/java/com/dietdoctorassignment/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dietdoctorassignment;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate; // <- add this necessary import
+import androidx.core.splashscreen.SplashScreen;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -8,6 +11,19 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+
+      @Override
+      protected void loadApp(String appKey) {
+        SplashScreen.installSplashScreen(MainActivity.this);
+        super.loadApp(appKey);
+      }
+    };
+  }
+
   @Override
   protected String getMainComponentName() {
     return "DietDoctorAssignment";
